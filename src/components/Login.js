e0ebb99330aae51f31f7cab884317b68e6b8c145
Vishal Soni <vs332837@gmail.com>
2023-04-25 00:39:21 +0530

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Login = (props) => {
 
-    const host = "http://localhost:5000" //we can make env file for these things
+    const host = "https://inotebookbackend5.onrender.com" //we can make env file for these things
 
 
 
